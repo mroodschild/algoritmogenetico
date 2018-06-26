8182f982b4fa175000c73cfc593775c10618f6cd
Matías Roodschild <mroodschild@gmail.com>
2018-06-25 22:20:39 -0300

    Test AG

---
+++ src/main/java/org/gitia/ag/GA.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gitia.ag;
+
+/**
+ *
+ * @author Matías Roodschild <mroodschild@gmail.com>
+ */
+public class GA {
+
+    public static void main(String[] args) {
+
+        // Set a candidate solution
+        FitnessCalc.setSolution("1111000000000000000000000001110000000000000000000000000000001111");
+
+        // Create an initial population
+        Population myPop = new Population(50, true);
+        
+        // Evolve our population until we reach an optimum solution
+        int generationCount = 0;
+        while (myPop.getFittest().getFitness() < FitnessCalc.getMaxFitness()) {
+            generationCount++;
+            System.out.println("Generation: " + generationCount + " Fittest: " + myPop.getFittest().getFitness());
+            myPop = Algorithm.evolvePopulation(myPop);
+        }
+        System.out.println("Solution found!");
+        System.out.println("Generation: " + generationCount);
+        System.out.println("Genes:");
+        System.out.println(myPop.getFittest());
+
+    }
+}
