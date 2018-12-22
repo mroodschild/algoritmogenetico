c30694bb3fa4943a53f6d310a761d7a0f9a0805a
matias <matias@matias-linux>
2018-12-22 18:30:04 -0300

    Agregamos selección de capas para mutar mutación en el bias quitamos el maxChange pusimos nos movemos un porcentaje del punto de origen (+-) * (r*0.5+0.15) + origen

--- src/main/java/org/gitia/ag/mutation/MultiNonUniformMutationPercentNeurona.java
+++ src/main/java/org/gitia/ag/mutation/MultiNonUniformMutationPercentNeurona.java
@@ -28,6 +28,8 @@ import org.gitia.ag.population.Individuo;
 public class MultiNonUniformMutationPercentNeurona {
 
     static double porcentaje = 0.05;
+    static int capaAMutar = 0;
+    static boolean mutBias = false;
 
     static Random r = new Random();
 
@@ -39,38 +41,43 @@ public class MultiNonUniformMutationPercentNeurona {
      * @param population_nocruzada // poblacion que no participo en el
      * cruzamiento
      * @param mutacion_size //cantidad de individuos a mutar
-     * @param val_min // valor minimo del dna
-     * @param val_max // valor maximo del dna
-     * @param genAct // epoca actual
-     * @param getMax // epoca total
      * @param neuronasCapas cantidad de neuronas que tiene determinada capa
      * @param pesosNeuronaCapa cantidad de pesos que tiene una neurona en
      * determinada capa
+     * @param porcentajeMutacion porcentaje de mutación
+     * @param capaSelMutar capa seleccionada para hacer la mutación
+     * @param mutarBias se mutarán los bias? true = si, false = no
      * @return un listado con los idividuos mutados
      */
     public static List<Individuo> mutacion(List<Individuo> population_nocruzada,
-            int mutacion_size, double val_min, double val_max, int genAct, int getMax,
-            int[] neuronasCapas, int[] pesosNeuronaCapa, double porcentajeMutacion
+            int mutacion_size, int[] neuronasCapas, int[] pesosNeuronaCapa,
+            double porcentajeMutacion, int capaSelMutar, boolean mutarBias
     ) {
         porcentaje = porcentajeMutacion;
-        //System.out.println("poblacion no cruzada:\t" + population_nocruzada.size());
+        capaAMutar = capaSelMutar;
+        mutBias = mutarBias;
+
+        //direccion de la mutación
         SimpleMatrix vdir = population_nocruzada.get(0).getDna().copy();
-        SimpleMatrix vmax = population_nocruzada.get(0).getDna().copy();
-        SimpleMatrix step = population_nocruzada.get(0).getDna().copy();
-        SimpleMatrix vPorcentaje = population_nocruzada.get(0).getDna().copy();
-        vdir.zero();
-        vPorcentaje.zero();
-        setPorcentaje(vPorcentaje, porcentaje, neuronasCapas, pesosNeuronaCapa);
-        setDir(vdir);
+        SimpleMatrix step = vdir.copy();
+        //máscara de mutación
+        SimpleMatrix vMaskMutation = vdir.copy();
+
         List<Individuo> aux = new ArrayList<>();
+        
         for (int i = 0; i < mutacion_size; i++) {
-            int idx = r.nextInt(population_nocruzada.size() - 1);
-            Individuo ind = population_nocruzada.get(idx);
+            int idx = r.nextInt(population_nocruzada.size());
+            Individuo ind = population_nocruzada.get(idx);//seleccionamos al azar algún individuo
+            //nos movemos izq o derecha?
             setDir(vdir);
-            setMaxChg(vmax, vdir, ind.getDna(), val_min, val_max);
-            step(step, genAct, getMax);
-            ind.setDna(sol(ind.getDna(), vdir, step, vmax, vPorcentaje));
+            //calculamos el paso
+            step(step);
+            //preparamos la máscara de mutación con los bias si son solicitados
+            selectLayer(vMaskMutation, porcentaje, neuronasCapas, pesosNeuronaCapa, capaAMutar, mutBias);
+            //nos movemos
+            ind.setDna(sol(ind.getDna(), vdir, step, vMaskMutation));
             aux.add(ind);
+            //reemplazamos al individuo
             population_nocruzada.set(idx, ind);
         }
         return aux;
@@ -78,46 +85,46 @@ public class MultiNonUniformMutationPercentNeurona {
 
     /**
      *
-     * @param vPorcentaje vector mascara para guardar los genes a mutar
-     * @param porcentajeMutacion
-     * @param first_gen posición inicial de la mutación
-     * @param last_gen posición final de la mutación
+     * @param vPorcentaje mascara para la mutación de los pesos de la neurona
+     * seleccionada
+     * @param porcentajeMutacion incida el porcentaje de mutación
+     * @param neuronasCapas vector de neuronas de la arquitectura
+     * @param pesosNeuronaCapa pesos que tienen las neuronas segun la capa
+     * @param layerSeleccionada capa de la que se seleccionará la neurona a
+     * mutar
      */
-    private static void setPorcentaje(
+    private static void selectLayer(
             SimpleMatrix vPorcentaje, double porcentajeMutacion,
-            int[] neuronasCapas, int[] pesosNeuronaCapa) {
-        int neuronas = neuronasCapas[0];// solo capa oculta
-        int pesos = pesosNeuronaCapa[0];
-        int neuronaSeleccionada = r.nextInt(neuronas);
-        int first_gen = neuronaSeleccionada * pesos;
-        int last_gen = first_gen + pesos;
-        for (int i = first_gen; i < last_gen; i++) {
-            if (r.nextDouble() < porcentajeMutacion) {
-                vPorcentaje.set(i, 1);
-            }
-        }
-    }
+            int[] neuronasCapas, int[] pesosNeuronaCapa, int layerSeleccionada, boolean mutBias) {
 
-    private static void setPorcentajeLastLayer(
-            SimpleMatrix vPorcentaje, double porcentajeMutacion,
-            int[] neuronasCapas, int[] pesosNeuronaCapa, double desviacion) {
-
-        int L = neuronasCapas.length;//capas
-        //int cantidadNeuronasCapasAnteriores = 0;
         int cantidadPesosCapasAnteriores = 0;
-        //neuronas antes de la capa de salida
-        for (int i = 0; i < L - 1; i++) {
-            //cantidadNeuronasCapasAnteriores += neuronasCapas[i];
-            cantidadPesosCapasAnteriores += neuronasCapas[i]*pesosNeuronaCapa[i];
+        int pesosTotales = 0;
+        //pesos anteriores a la capa seleccionada
+        for (int i = 0; i < layerSeleccionada; i++) {
+            cantidadPesosCapasAnteriores += neuronasCapas[i] * pesosNeuronaCapa[i];
         }
-        int neuronas = neuronasCapas[L - 1];// neuronas capa de salida
-        int pesos = pesosNeuronaCapa[L - 1];// Pesos por neurona capa salida
+        //calculamos el total de pesos antes de iniciar los Bias
+        for (int i = 0; i < neuronasCapas.length; i++) {
+            pesosTotales += neuronasCapas[i] * pesosNeuronaCapa[i];
+        }
+        int neuronas = neuronasCapas[layerSeleccionada];// neuronas capa de salida
+        int pesos = pesosNeuronaCapa[layerSeleccionada];// Pesos por neurona capa salida
         int neuronaSeleccionada = r.nextInt(neuronas);//seleccionada de la ultima capa
         int first_gen = neuronaSeleccionada * pesos + cantidadPesosCapasAnteriores;//posiciones para la ultima capa
         int last_gen = first_gen + pesos;//ultimo peso
         for (int i = first_gen; i < last_gen; i++) {
             if (r.nextDouble() < porcentajeMutacion) {
                 vPorcentaje.set(i, 1);
+            }
+        }
+        //mutamos los bias?
+        if (mutBias) {
+            int size = vPorcentaje.getNumElements();
+            //desde el ultimo peso
+            for (int i = pesosTotales; i < size; i++) {
+                if (r.nextDouble() < porcentajeMutacion) {
+                    vPorcentaje.set(i, 1);
+                }
             }
         }
     }
@@ -134,36 +141,31 @@ public class MultiNonUniformMutationPercentNeurona {
     }
 
     /**
-     * genera el vector de máximos cambios posibles
+     * El paso es de random * 0.5 + 0.15 , es decir garantizamos un movimiento
+     * de 0.15, con valores de 0.15 a 0.65
      *
-     * @param vMaxChg aqui se guarda
-     * @param dir direcciones generadas
-     * @param ind individuo
-     * @param max
-     * @param min
+     * @param step
      */
-    static public void setMaxChg(SimpleMatrix vMaxChg, SimpleMatrix dir,
-            SimpleMatrix ind, double min, double max) {
-        for (int i = 0; i < vMaxChg.getNumElements(); i++) {
-            if (dir.get(i) == 1) {
-                vMaxChg.set(i, max - ind.get(i));
-            } else {
-                vMaxChg.set(i, ind.get(i) - min);
-            }
-
-        }
-    }
-
-    static public void step(SimpleMatrix step, int genAct, int genMax) {
+    static public void step(SimpleMatrix step) {
         for (int i = 0; i < step.getNumElements(); i++) {
-            step.set(i, r.nextDouble() * (1 - (double) genAct / (double) genMax));
+            step.set(i, r.nextDouble() * 0.5 + 0.15);
         }
     }
 
-    static public SimpleMatrix sol(SimpleMatrix ind, SimpleMatrix dir, SimpleMatrix step, SimpleMatrix maxChange, SimpleMatrix vPorcentaje) {
+    /**
+     *
+     * La solución resultante es: a la posición actual se le mueven los pesos,
+     * según la máscara de mutación
+     *
+     * @param ind
+     * @param dir
+     * @param step
+     * @param vMaskMutation
+     * @return
+     */
+    static public SimpleMatrix sol(SimpleMatrix ind, SimpleMatrix dir, SimpleMatrix step, SimpleMatrix vMaskMutation) {
         SimpleMatrix sol = ind.copy();
-        sol = sol.plus(dir.elementMult(vPorcentaje).elementMult(step).elementMult(maxChange));
+        sol = sol.plus(dir.elementMult(vMaskMutation).elementMult(step));
         return sol;
     }
-
 }
